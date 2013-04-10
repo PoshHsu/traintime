e4b460d73ffa5cdef35397c5b6e2cd27c7f0584d
Patrick Wang <kk1fff@patrickz.net>
2013-04-10 15:29:28 +0800

    Add skelton of application server

---
+++ appserver/main.js
@@ -0,0 +1,17 @@
+var app = require('express')(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server);
+
+server.listen(45123);
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function (socket) {
+  console.log("received a connection");
+  socket.emit('hello', { "val": 'world' });
+  socket.on('hi', function (data) {
+    console.log(data);
+  });
+});
