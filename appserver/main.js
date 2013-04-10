0f0b05213da3d95f856eaf526c3e681c3e185990
Patrick Wang <kk1fff@patrickz.net>
2013-04-11 01:50:40 +0800

    Binding loopback ip in app server

--- appserver/main.js
+++ appserver/main.js
@@ -2,7 +2,7 @@ var app = require('express')(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server);
 
-server.listen(45123);
+server.listen(45123, '127.0.0.1');
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
