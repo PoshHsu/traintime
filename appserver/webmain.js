d7210bc767da24a82a0b2f211a3837456a794c85
Patrick Wang <kk1fff@patrickz.net>
2013-04-13 02:10:11 +0800

    Add content-type in response header

--- appserver/webmain.js
+++ appserver/webmain.js
@@ -8,30 +8,39 @@ web.start([
     "method": "get",
     "path": "/trainAt/:stationId/:direction",
     "handler": function(req, resp) {
-      fetcher.getTodayTrainOfStation(parseInt(req.params.stationId),
-                                     parseInt(req.params.direction),
-                                     function(result) {
-        resp.end(JSON.stringify(result));
-      }, function() { });
+      fetcher.getTodayTrainOfStation(
+        parseInt(req.params.stationId),
+        parseInt(req.params.direction),
+        function(result) {
+          resp.setHeader("content-type", "text/json; charset=utf-8");
+          resp.end(JSON.stringify(result));
+        },
+        function() { });
     }
   },
   {
     "method": "get",
     "path": "/train/:trainId",
     "handler": function(req, resp) {
-      fetcher.getTodayTrain(parseInt(req.params.trainId),
-                            function(result) {
-        resp.end(JSON.stringify(result));
-      }, function() { });
+      fetcher.getTodayTrain(
+        parseInt(req.params.trainId),
+        function(result) {
+          resp.setHeader("content-type", "text/json; charset=utf-8");
+          resp.end(JSON.stringify(result));
+        },
+        function() { });
     }
   },
   {
     "method": "get",
     "path": "/stations",
     "handler": function(req, resp) {
-      fetcher.getCityList(function(result) {
-        resp.end(JSON.stringify(result));
-      }, function() { });
+      fetcher.getCityList(
+        function(result) {
+          resp.setHeader("content-type", "text/json; charset=utf-8");
+          resp.end(JSON.stringify(result));
+        },
+        function() { });
     }
   }
 ], 8080);
